25b42b18549a5b778ebfcb593aa0510c99f1d879
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-01 02:51:32 -0700

    Populated the Clone, GetState, and SetState methods of CustomerProps.cs

--- MMABooksFramework2022/MMABooksProps/CustomerProps.cs
+++ MMABooksFramework2022/MMABooksProps/CustomerProps.cs
@@ -13,35 +13,63 @@ namespace MMABooksProps
     public class CustomerProps : IBaseProps
     {
         //Properties for the Customer
-        public string CustomerID { get; set; } = "";
+        public int CustomerID { get; set; } = 0;
 
         public string Name { get; set; } = "";
 
         public string Address { get; set; } = "";
 
+        public string City { get; set; } = "";
         public string State { get; set; } = "";
 
         public string ZipCode { get; set; } = "";
 
+        /// <summary>
+        /// ConcurrencyID. Don't manipulate directly.
+        /// </summary>
+        public int ConcurrencyID { get; set; } = 0;
 
         public object Clone()
         {
-            throw new NotImplementedException();
+            CustomerProps p = new CustomerProps();
+            p.CustomerID = this.CustomerID;
+            p.Name = this.Name;
+            p.Address = this.Address;
+            p.City = this.City;
+            p.State = this.State;
+            p.ZipCode = this.ZipCode;
+            p.ConcurrencyID = this.ConcurrencyID;
+            return p;
         }
 
         public string GetState()
         {
-            throw new NotImplementedException();
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(this);
+            return jsonString;
         }
 
         public void SetState(string jsonString)
         {
-            throw new NotImplementedException();
+            CustomerProps p = JsonSerializer.Deserialize<CustomerProps>(jsonString);
+            this.CustomerID = p.CustomerID;
+            this.Name = p.Name;
+            this.Address = p.Address;
+            this.City = p.City;
+            this.State = p.State;
+            this.ZipCode = p.ZipCode;
+            this.ConcurrencyID = p.ConcurrencyID;
         }
 
         public void SetState(DBDataReader dr)
         {
-            throw new NotImplementedException();
+            this.CustomerID = (int)dr["CustomerID"]);
+            this.Name = (string)dr["Name"];
+            this.Address = (string)dr["Address"];
+            this.City = (string)dr["City"];
+            this.State = (string)dr["State"];
+            this.ZipCode = (string)dr["ZipCode"];
+            this.ConcurrencyID = (Int32)dr["ConcurrencyID"];
         }
     }
 }
