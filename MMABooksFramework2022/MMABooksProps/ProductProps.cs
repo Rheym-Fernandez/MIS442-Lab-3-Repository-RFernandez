a7dcef39455dab8a1d86f5e62a4308e3d7cda4a1
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-01 18:03:03 -0700

    Created ProductProps Class.

---
+++ MMABooksFramework2022/MMABooksProps/ProductProps.cs
@@ -0,0 +1,42 @@
+﻿/*using MMABooksTools;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+using System;
+
+using MMABooksTools;
+using DBDataReader = MySql.Data.MySqlClient.MySqlDataReader;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MySql.Data.MySqlClient;
+
+namespace MMABooksProps
+{
+    internal class ProductProps : IBaseProps
+    {
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(DBDataReader dr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
