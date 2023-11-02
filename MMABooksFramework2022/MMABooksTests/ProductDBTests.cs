eb9adeea892ccfc08cbcad684e55beae8b264326
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-01 21:55:13 -0700

    Populated ProductDBTests Methods

--- MMABooksFramework2022/MMABooksTests/ProductDBTests.cs
+++ MMABooksFramework2022/MMABooksTests/ProductDBTests.cs
@@ -13,7 +13,86 @@ using MySql.Data.MySqlClient;
 namespace MMABooksTests
 {
     [TestFixture]
-    internal class ProductDBTests
+    public class ProductDBTests
     {
+        ProductDB db;
+        public void ResetData()
+        {
+            db = new ProductDB();
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_testingResetData";
+            command.CommandType = CommandType.StoredProcedure;
+            db.RunNonQueryProcedure(command);
+        }
+
+        [Test]
+        public void TestRetrieve()
+        {
+            ProductProps p = (ProductProps)db.Retrieve("ProductID");
+            Assert.AreEqual(737, p.ProductID);
+            Assert.AreEqual("A4CS", p.ProductCode);
+        }
+
+        [Test]
+        public void TestRetrieveAll()
+        {
+            List<ProductProps> list = (List<ProductProps>)db.RetrieveAll();
+            Assert.AreEqual(15, list.Count);
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            ProductProps p = (ProductProps)db.Retrieve("A4CS");
+            Assert.True(db.Delete(p));
+            Assert.Throws<Exception>(() => db.Retrieve("A4CS"));
+        }
+
+
+        [Test]
+        public void TestDeleteForeignKeyConstraint()
+        {
+            ProductProps p = (ProductProps)db.Retrieve("A4VB");
+            Assert.Throws<MySqlException>(() => db.Delete(p));
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            ProductProps p = (ProductProps)db.Retrieve("ADC4");
+            p.ProductCode = "Test";
+            Assert.True(db.Update(p));
+            p = (ProductProps)db.Retrieve("ADC4");
+            Assert.AreEqual("Test", p.ProductCode);
+        }
+
+        [Test]
+        public void TestUpdateFieldTooLong()
+        {
+            ProductProps p = (ProductProps)db.Retrieve("ADC4");
+            p.ProductCode = "Oregon is the state where Crater Lake National Park is abcdefghijklmopqrstuvwxyz.";
+            Assert.Throws<MySqlException>(() => db.Update(p));
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            ProductProps p = new ProductProps();
+            p.ProductID = 800;
+            p.ProductCode = "ABCD";
+            db.Create(p);
+            ProductProps p2 = (ProductProps)db.Retrieve(p.ProductID);
+            Assert.AreEqual(p.GetState(), p2.GetState());
+        }
+
+        [Test]
+        public void TestCreatePrimaryKeyViolation()
+        {
+            ProductProps p = new ProductProps();
+            p.ProductID = 10000;
+            p.ProductCode = "Oregon";
+            Assert.Throws<MySqlException>(() => db.Create(p));
+        }
     }
 }
+
