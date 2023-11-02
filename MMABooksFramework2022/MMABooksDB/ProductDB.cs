c31c65c930ca1220b9ec3f7ae0e34dc3923e63b0
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-01 20:20:41 -0700

    Started the ProductDB Class.

---
+++ MMABooksFramework2022/MMABooksDB/ProductDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using MMABooksTools;
+using MMABooksProps;
+
+using System.Data;
+
+// *** I use an "alias" for the ado.net classes throughout my code
+// When I switch to an oracle database, I ONLY have to change the actual classes here
+using DBBase = MMABooksTools.BaseSQLDB;
+using DBConnection = MySql.Data.MySqlClient.MySqlConnection;
+using DBCommand = MySql.Data.MySqlClient.MySqlCommand;
+using DBParameter = MySql.Data.MySqlClient.MySqlParameter;
+using DBDataReader = MySql.Data.MySqlClient.MySqlDataReader;
+using DBDataAdapter = MySql.Data.MySqlClient.MySqlDataAdapter;
+using DBDbType = MySql.Data.MySqlClient.MySqlDbType;
+using System.Collections.Generic;
+
+namespace MMABooksDB
+{
+    public class ProductDB : DBBase, IReadDB, IWriteDB
+    {
+        public IBaseProps Create(IBaseProps props)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IBaseProps props)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBaseProps Retrieve(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object RetrieveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IBaseProps props)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
