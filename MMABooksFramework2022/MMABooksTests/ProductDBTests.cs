704b0fb6bc8a4f86bde8bf1cb0de9046810ea84b
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-01 20:57:17 -0700

    Started ProductDBTests Class

---
+++ MMABooksFramework2022/MMABooksTests/ProductDBTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using MMABooksProps;
+using MMABooksDB;
+
+using DBCommand = MySql.Data.MySqlClient.MySqlCommand;
+using System.Data;
+
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    internal class ProductDBTests
+    {
+    }
+}
